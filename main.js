261249766f2ec73f0b80d66f1135b0b6dac04dc5
Heidi Hood <heidih74@gmail.com>
2020-11-29 12:47:32 -0500

    first user story

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 const { createStore } = Redux;
 
-// Your initial state, reducer, constants, action creators, and JS code should go here!
+const initialState = {
+  crewMembers: [],
+};
+
+//Reducer
+
+const crewMemberReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_CREW_MEMBER:
+      const newCrewArray = state.crewMembers.concat(action.newCrewMember);
+      return Object.assign({}, state, {
+        crewMembers: newCrewArray,
+      });
+    default:
+      return state;
+  }
+};
+
+const newCrewForm = document.getElementById("new-pirate-form");
+
+const ADD_CREW_MEMBER = "ADD_CREW_MEMBER";
+
+//Action creator
+
+const addCrewMember = (newCrewMember) => {
+  return {
+    type: ADD_CREW_MEMBER,
+    newCrewMember: newCrewMember,
+  };
+};
+
+//Event listener
+
+newCrewForm.addEventListener("submit", () => {
+  event.preventDefault();
+  const crewName = document.getElementById("name").value;
+  document.getElementById("name").value = "";
+  const newCrewMember = { name: crewName };
+  store.dispatch(addCrewMember(newCrewMember))
+});
+
+//Setup store
+const store = createStore(crewMemberReducer)
